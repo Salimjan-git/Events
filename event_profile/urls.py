01ebc18e36ab95da7d507f7641a57a2f3ff3a948
shahriyor <islomovrofe@gmail.com>
2025-12-16 16:56:58 +0500

    events_urls

--- event_profile/urls.py
+++ event_profile/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('events/', views.event_list, name='event_list'),
+    path('events/<int:pk>/', views.event_detail, name='event_detail'),
+    path('events/create/', views.event_create, name='event_create'),
+    path('categories/', views.category_list, name='category_list'),
+    path('companies/', views.company_list, name='company_list'),
+    path('profile/', views.profile_detail, name='profile_detail'),
 ]
